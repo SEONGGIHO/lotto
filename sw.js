da876f84d68335378c4c8aebf51fcf456a24a638
SEONGGIHO <87936308+SEONGGIHO@users.noreply.github.com>
2026-05-18 14:08:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lotto-balance-v9-20260518';
+const CACHE_NAME = 'lotto-balance-v10-20260518';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
